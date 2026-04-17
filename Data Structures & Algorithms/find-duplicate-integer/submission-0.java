aa0ae59b7d1131397a313502b55edf2ef10d9890
Garima Jain <43342486+GarimaBokdia@users.noreply.github.com>
2026-04-17 17:09:54 +0530

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return 0;
+        
+       Arrays.sort(nums);
+       for(int i=1;i<nums.length;i++){
+        if(nums[i] == nums[i-1])
+            return nums[i];
+       }
+
+       return 0;
+    }
+}
